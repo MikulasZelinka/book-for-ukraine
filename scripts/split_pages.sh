1c7f74919e8be6583be1ead4c159e49d15c93976
Mikuláš Zelinka <mzelinka@medallia.com>
2022-05-05 11:20:56 +0200

    data: preview pages

---
+++ scripts/split_pages.sh
@@ -0,0 +1,10 @@
+mv vitej_a_pojd_si_povidat_web_10-11.jpg z.jpg
+page=0
+for image in $(ls *.jpg)
+do
+	convert -crop 50%x100% ${image} %d.jpg
+	mv 0.jpg split/${page}.jpg
+	page=$((page+1))
+	mv 1.jpg split/${page}.jpg
+	page=$((page+1))
+done
