9e3891cefe220459347e32f8750347a6a7fcb652
Mikuláš Zelinka <mikulas@zelinka.dev>
2022-05-23 01:03:29 +0200

    refactor: ua → uk (language code for audio)

--- scripts/wav_to_mp3.sh
+++ scripts/wav_to_mp3.sh
@@ -4,20 +4,20 @@ IN=/mnt/e/code/svitej/public/resources/in/audio
 OUT=/mnt/e/code/svitej/public/resources/audio
 
 mkdir -p "$OUT"/cs
-mkdir -p "$OUT"/ua
+mkdir -p "$OUT"/uk
 
 cd "$IN"/cs
 for f in *.wav; do ffmpeg -i "$f" -vn -ar 44100 -ac 2 -q:a 2 "$OUT"/cs/"${f%.*}.mp3"; done
 
 cd "$IN"/ua
-for f in *.wav; do ffmpeg -i "$f" -vn -ar 44100 -ac 2 -q:a 2 "$OUT"/ua/"${f%.*}.mp3"; done
+for f in *.wav; do ffmpeg -i "$f" -vn -ar 44100 -ac 2 -q:a 2 "$OUT"/uk/"${f%.*}.mp3"; done
 
 # add the 'cs' prefix
 cd "$OUT"/cs
 mmv '*' 'cs_#1'
 
-cd "$OUT"/ua
+cd "$OUT"/uk
 # remove alternate takes
 rm *[0-9]\.*
 # rename the 'ua ' prefix
-mmv 'ua *' 'ua_#1'
+mmv 'ua *' 'uk_#1'
