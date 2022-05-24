54bbd50283af1fa7e4c562d57c19b6437b6fb349
Mikuláš Zelinka <mikulas@zelinka.dev>
2022-05-24 18:57:11 +0200

    refactor: split "about" into three columns

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,11 +2,11 @@
   import { _ } from "../i18n";
 </script>
 
-<section class="section is-medium">
+<section class="section is-medium pb-3">
   <div class="content">
     <div class="columns is-desktop" style="overflow:hidden;">
       <div class="column">
-        <h2 class="title" id="o-projektu">{$_("about")}</h2>
+        <h2 id="o-projektu">{$_("about")}</h2>
 
         <p>
           {$_("aboutSection.goal")}
@@ -19,6 +19,36 @@
         <p>
           {$_("aboutSection.support")}
         </p>
+      </div>
+      <div class="column">
+        <h2 id="tvurci">{$_("authors")}</h2>
+
+        <p>
+          {$_("authorsSection.content")}: Tereza Axmannová, tereza.axmann<span
+            style="display:none">name</span
+          >@gmail.com
+        </p>
+        <p>{$_("authorsSection.graphicDesign")}: Tereza Drnovcová</p>
+        <p>
+          {$_("authorsSection.typesetting")}: Martin Koukl,
+          martin.design.grafika<span style="display:none">name</span>@gmail.com
+        </p>
+        <p>{$_("authorsSection.editing")}: Eva Trčková</p>
+        <h4>{$_("authorsSection.translation")}</h4>
+        <ul>
+          <li>
+            <span class="fi fi-ua" />
+            {$_("languages.uk")}: Anastasia Litvincová
+          </li>
+        </ul>
+      </div>
+
+      <div class="column">
+        <h2>{$_("authorsSection.web")}</h2>
+        <p>
+          <a href="https://github.com/MikulasZelinka">Mikuláš Zelinka</a>,
+          mikulas<span style="display:none">name</span>@zelinka.dev
+        </p>
 
         <p>
           {$_("aboutSection.development")}
@@ -26,38 +56,27 @@
             ><i class="fa-brands fa-github" /> GitHub</a
           >.
         </p>
-      </div>
-      <div class="column">
-        <h2 class="title" id="tvurci">{$_("authors")}</h2>
+
+        <h4>{$_("authorsSection.translation")}</h4>
         <ul>
           <li>
-            {$_("authorsSection.web")}:
-            <a href="https://github.com/MikulasZelinka">Mikuláš Zelinka</a>,
-            mikulas<span style="display:none">name</span>@zelinka.dev
+            <span class="fi fi-gb" />
+            {$_("languages.en")}: Mikuláš Zelinka
           </li>
           <li>
-            {$_("authorsSection.content")}: Tereza Axmannová, tereza.axmann<span
-              style="display:none">name</span
-            >@gmail.com
+            <span class="fi fi-ua" />
+            {$_("languages.uk")}:
+            <a href="https://github.com/Legionivo">Andrey Konovka</a>,
+            <a href="https://lindat.cz/translation">Charles Translator</a>
           </li>
-          <li>
-            {$_("authorsSection.translation_uk")}: Anastasia Litvincová, Andrey
-            Konovka
-          </li>
-          <li>{$_("authorsSection.translation_en")}: Mikuláš Zelinka</li>
-          <li>{$_("authorsSection.graphicDesign")}: Tereza Drnovcová</li>
-          <li>
-            {$_("authorsSection.typesetting")}: Martin Koukl,
-            martin.design.grafika<span style="display:none">name</span
-            >@gmail.com
-          </li>
-          <li>{$_("authorsSection.editing")}: Eva Trčková</li>
         </ul>
-
-        <p>
-          {$_("authorsSection.acknowledgement")}
-        </p>
       </div>
     </div>
+  </div>
+</section>
+
+<section class="hero has-text-centered">
+  <div class="hero-body">
+    <p class="subtitle">{$_("authorsSection.acknowledgement")}</p>
   </div>
 </section>
