66cbadb4b9235c3affe8d494bfea8fb5c8b8b2b1
Mikuláš Zelinka <mikulas@zelinka.dev>
2022-06-09 16:21:13 +0200

    content: Svět knihy, ukrajinský stánek

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -2,8 +2,11 @@
 	<div class="hero-body pb-5">
 		<!-- <p class="title">Toto je ochutnávka připravovaného webu, který bude dostupný co nejdříve.</p> -->
 		<p class="subtitle">
-			V pátek 10. 6. 2022 nás můžete navštívit na <strong>
-				<a href="https://www.svetknihy.cz/">veletrhu Svět knihy v Praze</a></strong> – podrobnosti připravujeme.
+			V pátek 10. 6. 2022 nás můžete navštívit u <strong>
+				<a href="https://cesky.radio.cz/otevrenim-ukrajinskeho-stanku-zacal-veletrh-svet-knihy-8752831"
+					>ukrajinského stánku</a
+				></strong>
+			na <strong> <a href="https://www.svetknihy.cz/">veletrhu Svět knihy v Praze</a></strong>.
 		</p>
 	</div>
 </section>
